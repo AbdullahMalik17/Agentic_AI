186e43800345c677b80803ac2b82cfa4cc78e370
Abdullah Ather <abdullahather272@gmail.com>
2025-05-07 22:27:51 +0500

    Updated data resolving issues with chainlit

--- 02_chailit/src/02_chailit/app.py
+++ 02_chailit/src/02_chailit/app.py
@@ -5,5 +5,5 @@ async def main(message: cl.Message):
     # Our custom logic goes here...
     # Send a fake response back to the user
     await cl.Message(
-        content=f"Assalam o Alaikumd Received: {message.content}",
+        content=f"Received: {message.content}",
     ).send()
