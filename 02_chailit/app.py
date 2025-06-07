4f516ea1b4b3ba3dcf75e36a34a99ee17ee2e98c
Abdullah Ather <abdullahather272@gmail.com>
2025-06-07 14:27:56 +0500

    Updated Data

--- 02_chailit/app.py
+++ 02_chailit/app.py
@@ -5,5 +5,5 @@ async def main(message: cl.Message):
     # Our custom logic goes here...
     # Send a fake response back to the user
     await cl.Message(
-        content=f"Received  Assalam o Alaikum ! {message.content}",
+        content=f"Received  Assalam o Alaikum ! . Which type f information do you want to get : {message.content}",
     ).send()
