243ca2ff938036fcea2a48b8a242ce7a25d5e778
Abdullah Ather <abdullahather272@gmail.com>
2025-06-10 20:00:28 +0500

    Updated Data

--- OpenAI_hello/main.py
+++ OpenAI_hello/main.py
@@ -28,17 +28,33 @@ run_config = RunConfig(
 # Step 4: Create an agent
 agent = Agent(
     name="Hamna",
-    instructions="You are a sacharsit . Help people in teaching in short learning"
+    instructions="You are Hamna, a personal assistant. You can answer questions, provide information, and assist with various tasks. Always respond in a helpful and friendly manner.",
 )
+# It is used to show the display message in the chat 
+@cl.on_chat_start
+async def handle_message():
+    cl.user_session.set("History",[])
+    # Send a welcome message when the chat starts
+    await cl.Message(content="Hello! I am Hamna , Your personal assistant. How can I help you today?").send()
 
 @cl.on_message
 async def main(message: cl.Message):
+  try :  
+    history = cl.user_session.get("History",[])
+    # save the user Message in the hstroy .
+    history.append({"role":"user","content":message.content})
     result = await Runner.run(
         starting_agent = agent,
         input = message.content,
         run_config=run_config
     )
+    # Save the result in the histroy .
+    history.append({"role":"assistant","content":result.final_output})
+    cl.user_session.set("History",history)
     await cl.Message(content=result.final_output).send()
+  except Exception as e:
+    cl.Message(content={str(e)}).send()
+
  #   await cl.Message(content=result.final_output).send()  # Send the result back to the user
 
 
