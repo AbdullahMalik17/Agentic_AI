53803191378adc8ea2c4e0074ae580495ea6efca
Abdullah Ather <abdullahather272@gmail.com>
2025-08-05 19:47:31 +0500

    Updated data

--- Model_Context/README.md
+++ Model_Context/README.md
@@ -1,1 +1,11 @@
+# Context 
+Context basically means background data
+
+# Local Context
+It is a data that is not going to LLM directly and but it stores in our internal computer . 
+# How to do it .
+We use RunContextWrapper to rap it in the tool or function tool . 
+
 # Model Context 
+It is a data that is going to LLM directly and it stores in the LLM.
+It is given to LLM through chat history 
--- Model_Context/main.py
+++ Model_Context/main.py
@@ -1,11 +1,11 @@
-from agents import Agent , Runner , AsyncOpenAI , OpenAIChatCompletionsModel , set_tracing_disabled , RunConfig
+from agents import Agent , Runner , AsyncOpenAI , OpenAIChatCompletionsModel, RunConfig , RunContextWrapper , function_tool 
 import os 
 import asyncio
 from dotenv import load_dotenv , find_dotenv
+from dataclasses import dataclass
+
 # load the environment variables
 load_dotenv(find_dotenv())
-
-set_tracing_disabled(True)
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
@@ -25,15 +25,34 @@ run_config = RunConfig(
     model_provider = external_client,
     tracing_disabled=True,
 )    
+@dataclass 
+class Information:
+    name : str 
+    age :int 
+    email: str 
 
-agent = Agent(
-    name="Gemini Agent",
-    instructions="You are a helpful assistant that can answer questions and help with tasks."
-    )
+@function_tool
+async def get_information(Wrapper : RunContextWrapper)-> str:
+    print("Retrieving user information from context...")
+    print(f"Context received: {Wrapper.context}")
+    user_info = f"The name of user is {Wrapper.context.name}, age is {Wrapper.context.age}, and email is {Wrapper.context.email}."
+    print(f"Returning: {user_info}")
+    return user_info
+
 
 async def main():
-    result = await Runner.run(agent , "Why do we should learn AI especially Agentic AI ? ", run_config=run_config)
-    print(result.final_output )
+    # Create an agent with the model and function tool    
+    agent = Agent(
+        name = "InformationAgent",
+        tools=[get_information]
+    )    
 
+    user_info = Information("Abdullah",18,"muhammadabdullah51700@gmail.com")
+    print(f"Created user info: {user_info}")
+    result = await Runner.run(agent , "Please use the get_information tool to tell me about Abdullah's age", run_config=run_config , context=user_info)
+    print("Final output:")
+    print(result.final_output)
+if __name__ == "__main__":
+    asyncio.run(main())
 
-asyncio.run(main())    
+    
