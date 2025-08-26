71cf375a0e8356d3ceee4e81a235e57520a9cc96
Abdullah Ather <abdullahather272@gmail.com>
2025-08-26 22:51:14 +0500

    Updated Data

--- Guardrials/math_guardrials.py
+++ Guardrials/math_guardrials.py
@@ -2,6 +2,7 @@ from agents import Agent, RunContextWrapper , Runner , AsyncOpenAI , OpenAIChatC
 import os 
 from dotenv import load_dotenv , find_dotenv
 from pydantic import BaseModel 
+import asyncio
 
 # load the environment variables
 load_dotenv(find_dotenv())
@@ -18,25 +19,38 @@ external_client = AsyncOpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 model = OpenAIChatCompletionsModel(
-    model="gemini-2.5-pro",
+    model="gemini-2.5-flash",
     openai_client=external_client,
 )
 run_config = RunConfig(
     model = model ,
     model_provider = external_client,
-    workflow_name="Guardrials Workflow"
+    workflow_name="Guardrials  Math Workflow"
 )   
+agent_quardrial = Agent(
+    name = "Homework Quardrial Police",
+    instructions = "If the user is asking for help with math homework, you send is_homework as true, otherwise false. You always answer in the format {is_homework:bool,reasoning:str} and you never say anything else.",
+    model = model,
+    output_type = Math_Output
+)
 @input_guardrail
-def math_quardrials(ctx:RunContextWrapper,agent:Agent,input:str):
- return GuardrailFunctionOutput(
-     output_info="Math Output",
-     tripwire_triggered=False
- )
+async def math_quardrials(ctx:RunContextWrapper,agent:Agent,input:str):
+    result1 = await Runner.run(starting_agent=agent_quardrial,input=input, run_config=run_config)
+    return GuardrailFunctionOutput(
+       output_info="Math Output",
+       tripwire_triggered=result1.final_output.is_homework,
+    )
+    
 agent : Agent = Agent(
     name = "Guardrials Agent",
     instructions = "You are a helpful assistant that helps people find information.",
     model = model,
     input_guardrails=[math_quardrials],
 )
-result = Runner.run_sync(starting_agent=agent ,input = "What is your name? How can you help me ?", run_config=run_config)
-print(result.final_output)
+
+async def main():
+    result = await Runner.run(starting_agent=agent ,input = "Hi",run_config=run_config)
+    print(result.final_output)
+
+if __name__ == "__main__":
+    asyncio.run(main())
